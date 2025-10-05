422cec921e58244bb6e95e7fa920bfe275dd89a6
Mrbysco <Mrbysco@users.noreply.github.com>
2025-10-05 23:09:43 +0200

    Filter out mobs that don't have attack damage set (Closes #8)
    Since mob#doHurtTarget assumes it exists

--- src/main/java/com/buuz135/salem/item/TomeOfTheDamnedItem.java
+++ src/main/java/com/buuz135/salem/item/TomeOfTheDamnedItem.java
@@ -5,6 +5,7 @@ import com.buuz135.salem.SalemContent;
 import com.buuz135.salem.util.InventoryFinderUtil;
 import com.buuz135.salem.util.SalemRaidTier;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
@@ -28,7 +29,7 @@ public class TomeOfTheDamnedItem extends TrinketItem{
             ItemStack tome = InventoryFinderUtil.findFirst(player, SalemContent.TOME_OF_THE_DAMNED_CHARM.get());
             if (!tome.isEmpty()) {
                 List<LivingEntity> entityList = player.level().getEntitiesOfClass(LivingEntity.class, AABB.unitCubeFromLowerCorner(player.position()).inflate(5));
-                entityList.removeIf(entity -> entity.getId() == damage.getId() || entity instanceof Player);
+                entityList.removeIf(entity -> entity.getId() == damage.getId() || entity instanceof Player || entity.getAttribute(Attributes.ATTACK_DAMAGE) == null);
                 if (entityList.size() > 0){
                     entityList.get(player.level().random.nextInt(entityList.size())).doHurtTarget(damage);
                 }
